5e75eb474c54489b33fec7ac81a10d0b1b61a9f4
Aarón <Demenus@users.noreply.github.com>
2020-07-09 12:37:05 +0100

    Ejercicio 4

--- 4/src/Event.js
+++ 4/src/Event.js
@@ -1,3 +1,12 @@
-export default class Event{
-// implement your code here...
+export default class Event {
+    
+    constructor({second, type, message}) {
+        this.second = second;
+        this.type = type;
+        this.message = message;
+    }
+
+    execute() {
+        console.log(`> At second ${this.second}: ${JSON.stringify({type: this.type, message: this.message})}`)
+    }
 };
